20d2a4a6166579459026b332145f9b8717d5711c
Joy Singh <avnishmamta060606@gmail.com>
2022-02-03 17:34:56 +0530

    dev_branch

---
+++ demo_java/src/demo_java/main/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo_java.main;
+
+/**
+ * @author 91940
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
